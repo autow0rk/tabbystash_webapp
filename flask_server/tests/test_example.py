f790bfba21d2d85377d8d95cef9728127383b7a6
autow0rk <152362189+autow0rk@users.noreply.github.com>
2022-02-08 00:04:27 -0500

    Updated fixtures in test_example.py to tear down the User added to database after running test_insertingUser.py

--- flask_server/tests/test_example.py
+++ flask_server/tests/test_example.py
@@ -6,79 +6,34 @@ from src.extensions import db
 from src.models import User
 from flask import current_app
 
-# from src.configTest import TestingConfig
-
-# TODO: Database is not being created properly in tests -> fix
-
 
 @pytest.fixture
-def testingApp():
+def dummyApp():
     return create_app()
 
 
-# def setup_function():
-#     print("setting up")
+@pytest.fixture
+def dummyUser():
+    user = User(email="dummyemail@email.com", password="fakepassword")
+    return user
 
 
-# def test_myoutput(capsys):  # or use "capfd" for fd-level
-#     print("hello")
-#     with capsys.disabled():
-#         print("testing not captured")
-# sys.stderr.write("world\n")
-# captured = capsys.readouterr()
-# assert captured.out == "helo\n"
-# assert captured.err == "world\n"
-# print("next")
-# captured = capsys.readouterr()
-# assert captured.out == "next\n"
-# assert False
-# print("hello")
-# sys.stderr.write("world\n")
-# captured = capsys.readouterr()
-# assert captured.out == "hello\n"
-# assert captured.err == "world\n"
-# print("next")
-# captured = capsys.readouterr()
-# assert captured.out == "next\n"
+@pytest.fixture
+def database(dummyApp, dummyUser):
+    yield db
+    with dummyApp.app_context():
+        db.session.delete(dummyUser)
+        db.session.commit()
 
 
-# @pytest.fixture
-# def testingDb():
-#     return db
-
-
-def test_insertingUser(capsys, testingApp):
-
-    # allUsers = User.query.all()
-    # print("allUsers")
-    with capsys.disabled():
-        with testingApp.app_context():
-            print("the database url is: ", os.environ.get("TESTING_DB"))
-            print(User.query.all())
-            user = User(email="dummyemail@email.com", password="fakepassword")
-            db.session.add(user)
-            db.session.commit()
-
-            queriedUser = User.query.filter_by(
-                email="dummyemail@email.com", password="fakepassword"
-            ).first()
-            print(queriedUser.email, queriedUser.password)
-            assert queriedUser is not None
-            assert queriedUser.email == "dummyemail@email.com"
-            assert queriedUser.password == "fakepassword"
-
-            db.session.delete(queriedUser)
-            db.session.commit()
-
-        # user = User(email='dummyemail@email.com' password='fakepassword')
-        # deleteAllUnverifiedUsers()
-        # with testingApp.app_context():
-        #     print(os.environ.get("SQLALCHEMY_DATABASE_URI"))
-        #     # print("before applying config: ", testingApp.config)
-        #     # testingApp.config.from_object(Config)
-        #     # print("after applying config: ", testingApp.config)
-        #     print("hello!")
-    print("hi!")
-    # assert (1 + 1) == 2
-    # user = User(email='dummyemail@email.com' password='fakepassword')
-    # deleteAllUnverifiedUsers()
+def test_insertingUser(capsys, database, dummyUser, dummyApp):
+    with dummyApp.app_context():
+        database.session.add(dummyUser)
+        database.session.commit()
+        queriedUser = User.query.filter_by(
+            email="dummyemail@email.com", password="fakepassword"
+        ).first()
+        print(queriedUser.email, queriedUser.password)
+        assert queriedUser is not None
+        assert queriedUser.email == "dummyemail@email.com"
+        assert queriedUser.password == "fakepassword"
